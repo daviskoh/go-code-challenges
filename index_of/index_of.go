870d3a0cdbbd2e91bc08e01137261fea39c09046
daviskoh <koh.davis.0@gmail.com>
2014-10-27 13:25:53 -0400

    handle multi char substrings

--- index_of/index_of.go
+++ index_of/index_of.go
@@ -1,9 +1,13 @@
 package index_of
 
-func IndexOf(mainString, substring string) int {
-	if mainString == substring {
-		return 0
+func IndexOf(mainString, substring string, currentIndex int) int {
+	if currentIndex+len(substring) > len(mainString) {
+		return -1
 	}
 
-	return -1
+	if mainString[currentIndex:currentIndex+len(substring)] == substring {
+		return currentIndex
+	}
+
+	return IndexOf(mainString, substring, currentIndex+1)
 }
--- index_of/index_of_test.go
+++ index_of/index_of_test.go
@@ -1,6 +1,8 @@
 package index_of_test
 
 import (
+	"fmt"
+
 	. "github.com/daviskoh/go-code-challenges/index_of"
 
 	. "github.com/onsi/ginkgo"
@@ -14,10 +16,25 @@ import (
 
 var _ = Describe("IndexOf", func() {
 	It("should handle a simple case with a 1 char string", func() {
-		Expect(IndexOf("a", "a")).To(Equal(0))
+		Expect(IndexOf("a", "a", 0)).To(Equal(0))
 	})
 
 	It("should return -1 if string does not contain substring", func() {
-		Expect(IndexOf("a", "b")).To(Equal(-1))
+		Expect(IndexOf("a", "b", 0)).To(Equal(-1))
+	})
+
+	It("should handle a string with char > 1", func() {
+		Expect(IndexOf("ab", "b", 0)).To(Equal(1))
+		Expect(IndexOf("aabc", "b", 0)).To(Equal(2))
+	})
+
+	It("should handle multi char substrings", func() {
+		fmt.Println("***********************************")
+		fmt.Println("MULTI")
+		Expect(IndexOf("azbcasdf", "bc", 0)).To(Equal(2))
+
+		fmt.Println("***********************************")
+		fmt.Println("MULTI")
+		Expect(IndexOf("zzzzzzz", "bc", 0)).To(Equal(-1))
 	})
 })
