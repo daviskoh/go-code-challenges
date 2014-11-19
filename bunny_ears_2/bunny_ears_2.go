9e87574f5ff79f1c2871616951d03cf1db4c141d
daviskoh <koh.davis.0@gmail.com>
2014-11-19 11:15:28 -0500

    fin

--- bunny_ears_2/bunny_ears_2.go
+++ bunny_ears_2/bunny_ears_2.go
@@ -1,5 +1,13 @@
 package bunny_ears_2
 
-func BunnyEars2() {
+func BunnyEars2(bunnies int) int {
+	if bunnies == 0 {
+		return 0
+	}
 
+	if bunnies%2 == 0 {
+		return 3 + BunnyEars2(bunnies-1)
+	}
+
+	return 2 + BunnyEars2(bunnies-1)
 }
--- bunny_ears_2/bunny_ears_2_test.go
+++ bunny_ears_2/bunny_ears_2_test.go
@@ -7,6 +7,20 @@ import (
 	. "github.com/onsi/gomega"
 )
 
-var _ = Describe("BunnyEars2", func() {
+// We have bunnies standing in a line, numbered 1, 2, ... The odd bunnies (1, 3, ..) have the normal 2 ears.
+// The even bunnies (2, 4, ..) we'll say have 3 ears, because they each have a raised foot.
+// Recursively return the number of "ears" in the bunny line 1, 2, ... n (without loops or multiplication).
 
+var _ = Describe("BunnyEars2", func() {
+	It("should handle 0 bunnies", func() {
+		Expect(BunnyEars2(0)).To(Equal(0))
+	})
+
+	It("should handle a single odd numbered bunny", func() {
+		Expect(BunnyEars2(1)).To(Equal(2))
+	})
+
+	It("should handle a single odd & single even", func() {
+		Expect(BunnyEars2(2)).To(Equal(2 + 3))
+	})
 })
