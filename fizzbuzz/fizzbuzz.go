bf7804bdc1ac3a0eba55ac0de1cdc9c99adff347
daviskoh <koh.davis.0@gmail.com>
2014-11-25 20:14:16 -0500

    handle simple cases of fizzbuzz num check

---
+++ fizzbuzz/fizzbuzz.go
@@ -0,0 +1,21 @@
+package fizzbuzz
+
+import (
+	"strconv"
+)
+
+func FizzBuzz(x, y, n string) string {
+	nAsInt, _ := strconv.Atoi(n)
+	xAsInt, _ := strconv.Atoi(x)
+	yAsInt, _ := strconv.Atoi(y)
+
+	if nAsInt%xAsInt == 0 {
+		return "F"
+	}
+
+	if nAsInt%yAsInt == 0 {
+		return "B"
+	}
+
+	return n
+}
---
+++ fizzbuzz/fizzbuzz_test.go
@@ -0,0 +1,29 @@
+package fizzbuzz_test
+
+import (
+	. "github.com/daviskoh/go-code-challenges/fizzbuzz"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+/**
+ * func that checks value n against x & y:
+ * if divisble by x then return "F"
+ * if divisble by y then return "B"
+ * if divisble by both then return "FB"
+ */
+
+var _ = Describe("Fizzbuzz", func() {
+	It("handles non-matches", func() {
+		Expect(FizzBuzz("2", "3", "5")).To(Equal("5"))
+	})
+
+	It("handles a simple num divisible by x", func() {
+		Expect(FizzBuzz("2", "5", "4")).To(Equal("F"))
+	})
+
+	It("handles a simple num divisible by y", func() {
+		Expect(FizzBuzz("9", "3", "6")).To(Equal("B"))
+	})
+})
